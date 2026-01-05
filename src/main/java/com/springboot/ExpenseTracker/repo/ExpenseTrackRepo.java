2ef4162efc5c01557e4ebd2be3a023c0d446b0e6
Moogala-SaiRam <mugala.sairam@gmail.com>
2026-01-05 17:12:47 +0530

    updated Repo

--- src/main/java/com/springboot/ExpenseTracker/repo/ExpenseTrackRepo.java
+++ src/main/java/com/springboot/ExpenseTracker/repo/ExpenseTrackRepo.java
@@ -4,6 +4,13 @@ import com.springboot.ExpenseTracker.model.ExpenseTrack;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface ExpenseTrackRepo extends JpaRepository<ExpenseTrack,Integer> {
+    List<ExpenseTrack> findByDescriptionContaining(String keyword);
+    List<ExpenseTrack> findByDateBetween(LocalDate from,LocalDate to);
+
+    List<ExpenseTrack> findByDate(LocalDate date);
 }
