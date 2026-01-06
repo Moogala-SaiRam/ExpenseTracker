9a04fc23481efd9ab00818e6ba447c2508c2c722
Moogala-SaiRam <mugala.sairam@gmail.com>
2026-01-06 12:39:34 +0530

    Updated Service

--- src/main/java/com/springboot/ExpenseTracker/service/ExpenseTrackService.java
+++ src/main/java/com/springboot/ExpenseTracker/service/ExpenseTrackService.java
@@ -1,11 +1,14 @@
 package com.springboot.ExpenseTracker.service;
 
 import com.springboot.ExpenseTracker.model.ExpenseTrack;
+import com.springboot.ExpenseTracker.model.dto.ExpenseRequestDTO;
+import com.springboot.ExpenseTracker.model.dto.ExpenseResponseDTO;
 import com.springboot.ExpenseTracker.repo.ExpenseTrackRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -14,21 +17,48 @@ public class ExpenseTrackService {
     @Autowired
     private ExpenseTrackRepo repo;
 
-    public List<ExpenseTrack> getAllExpenses() {
-        return repo.findAll();
+    public List<ExpenseResponseDTO> getAllExpenses() {
+        List<ExpenseTrack> expensesList = repo.findAll();
+        List<ExpenseResponseDTO> expenseResponseDTOList = new ArrayList<>();
+
+        for(ExpenseTrack track : expensesList){
+            ExpenseResponseDTO responseDTO = new ExpenseResponseDTO(
+                    track.getId(),
+                    track.getAmount(),
+                    track.getDate(),
+                    track.getDescription()
+            );
+            expenseResponseDTOList.add(responseDTO);
+        }
+
+        return expenseResponseDTOList;
     }
-    public ExpenseTrack getById(int id) {
-        return repo.findById(id).orElse(null);
+    public ExpenseResponseDTO getById(int id) {
+        ExpenseTrack expense = repo.findById(id).orElse(null);
+        ExpenseResponseDTO responseDTO = new ExpenseResponseDTO(
+                expense.getId(),
+                expense.getAmount(),
+                expense.getDate(),
+                expense.getDescription()
+        );
+        return responseDTO;
     }
 
-    public ExpenseTrack addOrUpdateExpense(ExpenseTrack expenseTrack) {
-        ExpenseTrack track = new ExpenseTrack();
-        track.setId(expenseTrack.getId());
-        track.setAmount(expenseTrack.getAmount());
-        track.setDate(expenseTrack.getDate());
-        track.setDescription(expenseTrack.getDescription());
-        repo.save(track);
-        return track;
+    public ExpenseResponseDTO addExpense(ExpenseRequestDTO expenseRequest) {
+        // ExpenseRequest --> Entity
+        ExpenseTrack expense = new ExpenseTrack();
+        expense.setAmount(expenseRequest.amount());
+        expense.setDate(expenseRequest.date());
+        expense.setDescription(expenseRequest.description());
+        ExpenseTrack savedExpense = repo.save(expense);
+        //Entity --> ExpenseResponse
+        ExpenseResponseDTO responseDTO = new ExpenseResponseDTO(
+                savedExpense.getId(),
+                savedExpense.getAmount(),
+                savedExpense.getDate(),
+                savedExpense.getDescription()
+        );
+        return responseDTO;
     }
 
 
@@ -37,27 +67,75 @@ public class ExpenseTrackService {
         return "Deleted";
     }
 
-    public List<ExpenseTrack> searchByKey(String keyword) {
-        return repo.findByDescriptionContaining(keyword);
+    public List<ExpenseResponseDTO> searchByKey(String keyword) {
+        List<ExpenseTrack> expensesList = repo.findByDescriptionContaining(keyword);
+        List<ExpenseResponseDTO> expenseResponseDTOList = new ArrayList<>();
+
+        for(ExpenseTrack track : expensesList){
+            ExpenseResponseDTO responseDTO = new ExpenseResponseDTO(
+                    track.getId(),
+                    track.getAmount(),
+                    track.getDate(),
+                    track.getDescription()
+            );
+            expenseResponseDTOList.add(responseDTO);
+        }
+
+        return expenseResponseDTOList;
     }
 
     public List<ExpenseTrack> saveAll(List<ExpenseTrack> expenses) {
         return repo.saveAll(expenses);
     }
 
-    public List<ExpenseTrack> searchBetweenDate(LocalDate from, LocalDate to) {
-        return repo.findByDateBetween(from,to);
+    public List<ExpenseResponseDTO> searchBetweenDate(LocalDate from, LocalDate to) {
+        List<ExpenseTrack> expensesList = repo.findByDateBetween(from,to);
+        List<ExpenseResponseDTO> expenseResponseDTOList = new ArrayList<>();
+
+        for(ExpenseTrack track : expensesList){
+            ExpenseResponseDTO responseDTO = new ExpenseResponseDTO(
+                    track.getId(),
+                    track.getAmount(),
+                    track.getDate(),
+                    track.getDescription()
+            );
+            expenseResponseDTOList.add(responseDTO);
+        }
+
+        return expenseResponseDTOList;
     }
 
-    public List<ExpenseTrack> searchByDate(LocalDate date) {
-        return repo.findByDate(date);
+    public List<ExpenseResponseDTO> searchByDate(LocalDate date) {
+        List<ExpenseTrack> expensesList = repo.findByDate(date);
+        List<ExpenseResponseDTO> expenseResponseDTOList = new ArrayList<>();
+
+        for(ExpenseTrack track : expensesList){
+            ExpenseResponseDTO responseDTO = new ExpenseResponseDTO(
+                    track.getId(),
+                    track.getAmount(),
+                    track.getDate(),
+                    track.getDescription()
+            );
+            expenseResponseDTOList.add(responseDTO);
+        }
+
+        return expenseResponseDTOList;
     }
-    public ExpenseTrack updateById(ExpenseTrack expenseTrack,int id){
-        ExpenseTrack track = repo.findById(id).get();
-        track.setAmount(expenseTrack.getAmount());
-        track.setDate(expenseTrack.getDate());
-        track.setDescription(expenseTrack.getDescription());
-        repo.save(track);
-        return track;
+    public ExpenseResponseDTO updateById(ExpenseRequestDTO requestDTO,int id){
+        ExpenseTrack expense = repo.findById(id).orElse(null);
+
+        expense.setAmount(requestDTO.amount());
+        expense.setDate(requestDTO.date());
+        expense.setDescription(requestDTO.description());
+
+        ExpenseTrack savedExpense = repo.save(expense);
+        ExpenseResponseDTO responseDTO = new ExpenseResponseDTO(
+                savedExpense.getId(),
+                savedExpense.getAmount(),
+                savedExpense.getDate(),
+                savedExpense.getDescription()
+        );
+        return responseDTO;
+
     }
 }
